ab0cf0dd2f4ca69d56c62d3e7ec56fd87dcb2381
Jimmy Carrera <jimy.carrera@utelvt.edu.ec>
2022-09-08 14:16:03 -0500

    TABLA DE PERSONA

---
+++ datos.sql
@@ -0,0 +1,4 @@
+create database miplataforma;
+use miplataforma;
+
+create table persona(idpersona int, nombres varchar(20), apellidos varchar(20), cedula varchar(10), idgenero int, fechanacimiento date, idnacionalidad int, idetnia int, idestadocivil int);
